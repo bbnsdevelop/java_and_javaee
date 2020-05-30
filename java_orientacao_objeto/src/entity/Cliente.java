09fe961f779832d408fa283f22aab8ae50373b07
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-30 17:51:26 -0300

    criando uma classe cliente com modificador publico

---
+++ java_orientacao_objeto/src/entity/Cliente.java
@@ -0,0 +1,18 @@
+//localização do pacote
+package entity;
+
+//declaração da Classe
+//Nome da Classe Cliente deve ser o nome do arquivo Cliente.java
+//Deve ser uma classe de acesso publico
+public class Cliente {
+	
+	//Atributos -> dados pertencentes a Classe
+	//Visibilidade - tipo - nome
+	//public -> acesso total ao elemento
+	//tipos de dados (primitivos e wrappers (Classes))
+	public Integer idCliente;
+	public String nome;
+	public String email;
+	
+	
+}
--- java_orientacao_objeto/src/main/Main.java
+++ java_orientacao_objeto/src/main/Main.java
@@ -1,12 +1,25 @@
 package main;
 
+import entity.Cliente;
+
 //Classe utilizada para exeutar o projeto Java local
 public class Main {
+	
 	//Em java, para executar uma Classe, é necessário um
 	//método com a assinatura: static void main
 	public static void main(String[] args) {
-		// imprimir mensagem no console
-		// syso -> ctrl + espaço
-		System.out.println("Bem vindo Java - Turma");
+
+		//Classe -> Objeto
+		Cliente c1 = new Cliente();
+		c1.idCliente = 1;
+		c1.nome = "Sergio Mendes";
+		c1.email = "sergio.coti@gmail.com";
+		System.out.println("Id.....: " + c1.idCliente);
+		System.out.println("Nome...: " + c1.nome);
+		System.out.println("Email..: " + c1.email);
+		//imprimir mensagem no console
+		//syso -> ctrl + espaço
+		System.out.println("Bem vindo ao Java - Turma BRQ COTI");
+		
 	}
 }
