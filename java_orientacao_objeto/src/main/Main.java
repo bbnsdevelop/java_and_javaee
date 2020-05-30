9fdc5f0b9abe26c05f2aa2d00334c20167adcb15
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-30 17:47:40 -0300

    criando projeto para estudar orientação a objeto

---
+++ java_orientacao_objeto/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+//Classe utilizada para exeutar o projeto Java local
+public class Main {
+	//Em java, para executar uma Classe, é necessário um
+	//método com a assinatura: static void main
+	public static void main(String[] args) {
+		// imprimir mensagem no console
+		// syso -> ctrl + espaço
+		System.out.println("Bem vindo Java - Turma");
+	}
+}
