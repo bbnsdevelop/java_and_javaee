b220bc2f5cb9a1abeee46e16a8173afb465b1a4e
bbnsdevelop <bbnsdevelop@gmail.com>
2020-05-31 17:36:10 -0300

    criando uma entidade pessoa

---
+++ java_orientacao_objeto/src/entity/Pessoa.java
@@ -0,0 +1,37 @@
+package entity;
+
+public class Pessoa {
+
+	private Integer idPessoa;
+	private String nome;
+
+	public Pessoa() {
+	}
+
+	public Pessoa(Integer idPessoa, String nome) {
+		this.idPessoa = idPessoa;
+		this.nome = nome;
+	}
+
+	public Integer getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Integer idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [idPessoa=" + idPessoa + ", nome=" + nome + "]";
+	}
+
+}
